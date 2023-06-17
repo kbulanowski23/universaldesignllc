470fe10c9bfb8f029ae2efb058a76519ebe82e70
Konrad Bulanowski <konradbulanowski@Konrads-MacBook-Pro.local>
2023-06-17 14:55:07 -0400

    home page with nav bar and slider

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'KonradBulanowskiResume'`, () => {
+  it(`should have as title 'Universal Design LLC'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('KonradBulanowskiResume');
+    expect(app.title).toEqual('Universal Design LLC');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('KonradBulanowskiResume app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('Universal Design LLC app is running!');
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+// import { Slick } from "ngx-slickjs";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,50 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'KonradBulanowskiResume';
+  title = 'Universal Design LLC';
+
+  images = [  
+    { img: "../assets/images/1.jpeg" },  
+    { img: "../assets/images/2.jpeg" },  
+    { img: "../assets/images/3.jpeg" },  
+    { img: "../assets/images/4.jpeg" },  
+    { img: "../assets/images/5.jpeg" },  
+    { img: "../assets/images/6.jpeg" },  
+  ];  
+  
+  // slickConfig: Slick.Config = {
+  //   infinite: true,
+  //   slidesToShow: 4,
+  //   slidesToScroll: 2,
+  //   dots: true,
+  //   autoplay: true,
+  //   autoplaySpeed: 2000
+  // };
+
+  slideConfig = { 
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    dots: true,
+    autoplay: true,
+    autoplaySpeed: 4000
+  };
+  addSlide() {
+    // this.slides.push({ img: 'http://placehold.it/350x150/777777' });
+  }
+  removeSlide() {
+    this.images.length = this.images.length - 1;
+  }
+  slickInit(e: any) {
+    console.log('slick initialized');
+  }
+  breakpoint(e: any) {
+    console.log('breakpoint');
+  }
+  afterChange(e: any) {
+    console.log('afterChange');
+  }
+  beforeChange(e: any) {
+    console.log('beforeChange');
+  }
 }
