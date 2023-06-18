0d24df175f2b70dc51dc700061ba760576747cf1
Konrad Bulanowski <konradbulanowski@Konrads-MacBook-Pro.local>
2023-06-18 14:19:05 -0400

    interior layout is done

--- src/app/front-facade/front-facade.component.ts
+++ src/app/front-facade/front-facade.component.ts
@@ -7,41 +7,13 @@ import { Lightbox } from 'ngx-lightbox';
   styleUrls: ['./front-facade.component.css']
 })
 export class FrontFacadeComponent {
-  // private _album: Array<any> = [];
-  // constructor(private _lightbox: Lightbox) {
-  //   for (let i = 1; i <= 4; i++) {
-  //     const src = 'src/assets/images/FrontFacade/entrance' + i + '.jpeg';
-  //     const caption = 'Image ' + i + ' caption here';
-  //     const thumb = 'src/assets/images/FrontFacade/entrance' + i + '.jpg';
-  //     const album = {
-  //        src: src,
-  //        caption: caption,
-  //        thumb: thumb
-  //     };
-
-  //     this._album.push(album);
-  //   }
-  // }
-
-  // open(index: number): void {
-  //   // open lightbox
-  //   this._lightbox.open(this._album, index);
-  // }
-
-  // close(): void {
-  //   // close lightbox programmatically
-  //   this._lightbox.close();
-  // }
-
   public frontFacade: Array<any> = [];
   constructor(private _lightbox: Lightbox) {
     for (let i = 1; i <= 22; i++) {
       const src = 'assets/images/FrontFacade/entrance' + i + '.jpeg';
-      const caption = 'Image ' + i + ' caption here';
       const thumb = 'assets/images/FrontFacade/entrance' + i + '.jpeg';
       const album = {
          src: src,
-         caption: caption,
          thumb: thumb
       };
 
--- src/app/interior/interior.component.ts
+++ src/app/interior/interior.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Lightbox } from 'ngx-lightbox';
 
 @Component({
   selector: 'app-interior',
@@ -6,5 +7,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./interior.component.css']
 })
 export class InteriorComponent {
+  public interior: Array<any> = [];
+  constructor(private _lightbox: Lightbox) {
+    for (let i = 1; i <= 22; i++) {
+      const src = 'assets/images/Interior/interior' + i + '.jpeg';
+      const thumb = 'assets/images/Interior/interior' + i + '.jpeg';
+      const album = {
+         src: src,
+         thumb: thumb
+      };
 
+      this.interior.push(album);
+    }
+  }
+
+  open(index: number): void {
+    // open lightbox
+    this._lightbox.open(this.interior, index);
+  }
+
+  close(): void {
+    // close lightbox programmatically
+    this._lightbox.close();
+  }
 }
